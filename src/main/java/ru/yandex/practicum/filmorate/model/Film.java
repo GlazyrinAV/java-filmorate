1b20e61521d747a596e80ea559fa11043098d564
AVG <alex.glazyrin86@mail.ru>
2023-04-01 22:31:18 +0300

    first commit

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,38 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
-import ru.yandex.practicum.filmorate.adapters.DurationDesirializator;
-import ru.yandex.practicum.filmorate.adapters.DurationSerializator;
-import ru.yandex.practicum.filmorate.customConstraints.DurationConstraint;
-import ru.yandex.practicum.filmorate.customConstraints.ReleaseDateConstraint;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
-@RequiredArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Film {
-    @NotBlank(message = "Неверно указано название фильма.")
-    private final String name;
-
-    @Size(max = 200, message = "Описание не может быть больше 200 символов.")
-    private final String description;
-
-    @ReleaseDateConstraint
-    private final LocalDate releaseDate;
-
-    @JsonDeserialize(using = DurationDesirializator.class)
-    @JsonSerialize(using = DurationSerializator.class)
-    @DurationConstraint
-    private final Duration duration;
-
-    private int id;
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,33 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
-import ru.yandex.practicum.filmorate.customConstraints.WhiteSpaceConstraint;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
-@RequiredArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class User {
-
-    @Email(message = "Неверно указан электронный адрес.")
-    @NotBlank(message = "Неверно указан электронный адрес.")
-    private final String email;
-
-    @NotBlank(message = "Неверно указан логин.")
-    @WhiteSpaceConstraint
-    private final String login;
-
-    private String name;
-
-    @Past(message = "Неверно указана дата рождения.")
-    private final LocalDate birthday;
-
-    private int id;
+    int id;
+    String email;
+    String login;
+    String name;
+    LocalDate birthday;
 }
