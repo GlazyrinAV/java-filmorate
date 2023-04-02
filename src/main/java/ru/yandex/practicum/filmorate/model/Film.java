07da7e05800cec36bbede41948962fa0fd414919
AVG <alex.glazyrin86@mail.ru>
2023-04-02 17:06:48 +0300

    first commit

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,15 +1,21 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
 public class Film {
-    int id;
-    String name;
-    String description;
-    LocalDate releaseDate;
-    Duration duration;
+    @EqualsAndHashCode.Include
+    private final int id;
+    @EqualsAndHashCode.Exclude
+    private final String name;
+    @EqualsAndHashCode.Exclude
+    private final String description;
+    @EqualsAndHashCode.Exclude
+    private final LocalDate releaseDate;
+    @EqualsAndHashCode.Exclude
+    private final Duration duration;
 }
