a7f4f27e1861a84372eb3a61857dcec5702bc81a
AVG <alex.glazyrin86@mail.ru>
2023-04-02 23:10:23 +0300

    первая рабочая версия с пройденными тестами

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +11,19 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class Film {
 
+    @NotBlank(message = "Неверно указано название фильма.")
     @EqualsAndHashCode.Exclude
     private final String name;
+
     @EqualsAndHashCode.Exclude
     private final String description;
+
     @EqualsAndHashCode.Exclude
     private final LocalDate releaseDate;
+
     @EqualsAndHashCode.Exclude
     private final int duration;
+
     @EqualsAndHashCode.Include
     private int id;
 
