16c3c9f8870acb5c0c689d8f88e7518c1d7283c6
AVG <glazyrinav1986@yandex.ru>
2023-04-03 14:28:58 +0300

    minor changes

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -20,17 +20,17 @@ import static org.springframework.http.HttpStatus.*;
 public class FilmController {
 
     private static int idFilmSequence = 1;
-    private final LocalDate FIRST_FILM = LocalDate.of(1895, Month.DECEMBER, 28);
+    private final LocalDate FIRSTFILM = LocalDate.of(1895, Month.DECEMBER, 28);
     private final HashMap<Integer, Film> films = new HashMap<>();
 
     @PostMapping("/films")
     public ResponseEntity<Film> addNewFilm(@Valid @RequestBody Film film) {
         log.info("Получен запрос на создание фильма.");
-        if (!films.containsValue(film) && film.getReleaseDate().isAfter(FIRST_FILM)) {
+        if (!films.containsValue(film) && film.getReleaseDate().isAfter(FIRSTFILM)) {
             film.setId(setNewId());
             films.put(film.getId(), film);
             return new ResponseEntity<>(film, CREATED);
-        } else if (!films.containsValue(film) && film.getReleaseDate().isBefore(FIRST_FILM)) {
+        } else if (!films.containsValue(film) && film.getReleaseDate().isBefore(FIRSTFILM)) {
             log.info("Дата выхода фильма не может быть раньше 28.12.1895.");
             return new ResponseEntity<>(film, BAD_REQUEST);
         } else {
@@ -42,10 +42,10 @@ public class FilmController {
     @PutMapping("/films")
     public ResponseEntity<Film> updateFilm(@Valid @RequestBody Film film) {
         log.info("Получен запрос на обновление фильма.");
-        if (films.containsKey(film.getId()) && film.getReleaseDate().isAfter(FIRST_FILM)) {
+        if (films.containsKey(film.getId()) && film.getReleaseDate().isAfter(FIRSTFILM)) {
             films.replace(film.getId(), film);
             return new ResponseEntity<>(film, OK);
-        } else if (films.containsValue(film) && film.getReleaseDate().isBefore(FIRST_FILM)) {
+        } else if (films.containsValue(film) && film.getReleaseDate().isBefore(FIRSTFILM)) {
             log.info("Дата выхода фильма не может быть раньше 28.12.1895.");
             return new ResponseEntity<>(film, BAD_REQUEST);
         } else {
