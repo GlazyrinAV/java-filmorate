0bc7e1edd54dac08f2f1681e0a84d0e18ed3c542
AVG <glazyrinav1986@yandex.ru>
2023-04-03 14:12:49 +0300

    minor changes

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controllers;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -14,11 +15,12 @@ import java.util.HashMap;
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
+@Validated
 @Slf4j
 public class FilmController {
 
-    private final LocalDate FIRST_FILM = LocalDate.of(1895, Month.DECEMBER, 28);
     private static int idFilmSequence = 1;
+    private final LocalDate FIRST_FILM = LocalDate.of(1895, Month.DECEMBER, 28);
     private final HashMap<Integer, Film> films = new HashMap<>();
 
     @PostMapping("/films")
@@ -28,6 +30,9 @@ public class FilmController {
             film.setId(setNewId());
             films.put(film.getId(), film);
             return new ResponseEntity<>(film, CREATED);
+        } else if (!films.containsValue(film) && film.getReleaseDate().isBefore(FIRST_FILM)) {
+            log.info("Дата выхода фильма не может быть раньше 28.12.1895.");
+            return new ResponseEntity<>(film, BAD_REQUEST);
         } else {
             log.info("Такой фильм уже существует.");
             return new ResponseEntity<>(film, BAD_REQUEST);
@@ -40,6 +45,9 @@ public class FilmController {
         if (films.containsKey(film.getId()) && film.getReleaseDate().isAfter(FIRST_FILM)) {
             films.replace(film.getId(), film);
             return new ResponseEntity<>(film, OK);
+        } else if (films.containsValue(film) && film.getReleaseDate().isBefore(FIRST_FILM)) {
+            log.info("Дата выхода фильма не может быть раньше 28.12.1895.");
+            return new ResponseEntity<>(film, BAD_REQUEST);
         } else {
             return new ResponseEntity<>(film, NOT_FOUND);
         }
