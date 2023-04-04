94c848df9e24f8942ab26f7fc4df852cbf147d87
AVG <glazyrinav1986@yandex.ru>
2023-04-04 08:52:07 +0300

    feat - изменено хранилище фильмов и юзеров - изменены анотации в классах Film и User

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -12,22 +12,18 @@ import java.time.LocalDate;
 
 @Data
 @RequiredArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Film {
     @NotBlank(message = "Неверно указано название фильма.")
-    @EqualsAndHashCode.Exclude
     private final String name;
 
     @Size(max = 200, message = "Описание не может быть больше 200 символов.")
-    @EqualsAndHashCode.Exclude
     private final String description;
 
-    @EqualsAndHashCode.Exclude
     private final LocalDate releaseDate;
 
     @Positive(message = "Неверно указана длительность фильма.")
-    @EqualsAndHashCode.Exclude
     private final int duration;
 
-    @EqualsAndHashCode.Include
     private int id;
 }
