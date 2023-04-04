9fc0de01d53ddce5cf53097d6ab2e893f2ce97e4
AVG <glazyrinav1986@yandex.ru>
2023-04-04 10:47:25 +0300

    feat - изменено хранилище фильмов и юзеров - изменены анотации в классах Film и User - продолжительность заменена на Duration - добавлены собственные ограничения для User и Film

--- src/main/java/ru/yandex/practicum/filmorate/customConstraints/ReleaseDateConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/customConstraints/ReleaseDateConstraintValidator.java
@@ -7,15 +7,17 @@ import java.time.Month;
 
 public class ReleaseDateConstraintValidator implements ConstraintValidator<ReleaseDateConstraint, LocalDate> {
 
-    private final LocalDate firstFilm = LocalDate.of(1895, Month.DECEMBER, 28);
-
     @Override
     public void initialize(ReleaseDateConstraint constraintAnnotation) {
+
         ConstraintValidator.super.initialize(constraintAnnotation);
+
     }
 
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
-        return localDate.isAfter(firstFilm);
+
+        return localDate.isAfter(LocalDate.of(1895, Month.DECEMBER, 28));
+
     }
 }
