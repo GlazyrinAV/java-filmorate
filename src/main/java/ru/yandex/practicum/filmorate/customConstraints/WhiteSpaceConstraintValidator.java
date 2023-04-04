74e23f55a92e8f2b1ab82fc88aef333ebeed281b
AVG <glazyrinav1986@yandex.ru>
2023-04-04 10:36:56 +0300

    feat - изменено хранилище фильмов и юзеров - изменены анотации в классах Film и User - продолжительность заменена на Duration - добавлены собственные ограничения для User и Film

--- src/main/java/ru/yandex/practicum/filmorate/customConstraints/WhiteSpaceConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/customConstraints/WhiteSpaceConstraintValidator.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.customConstraints;
 
-import ru.yandex.practicum.filmorate.model.User;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
