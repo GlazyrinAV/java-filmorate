c9c7302e12ccf7d1226d08f4ef58168d353e41ef
AVG <alex.glazyrin86@mail.ru>
2023-04-16 22:29:16 +0300

    создана новая структура проекта

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -8,22 +8,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler ({UserAlreadyExistsException.class, FilmAlreadyExistsException.class})
+    @ExceptionHandler({UserAlreadyExistsException.class, FilmAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse UserAlreadyExistsException(RuntimeException exception) {
+    public ErrorResponse userAlreadyExistsException(RuntimeException exception) {
         return new ErrorResponse("error", exception.getMessage());
     }
 
-    @ExceptionHandler ({UserNotFoundException.class, FilmNotFoundException.class})
+    @ExceptionHandler({UserNotFoundException.class, FilmNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse UserNotFoundException(RuntimeException exception) {
+    public ErrorResponse userNotFoundException(RuntimeException exception) {
         return new ErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse ValidationException(ValidationException exception) {
+    public ErrorResponse validationException(ValidationException exception) {
         return new ErrorResponse("error", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse illegalArgumentException(IllegalArgumentException exception) {
+        return new ErrorResponse("error", "Получен неподходящий аргумент или аргумент неправильного типа");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse otherException(RuntimeException exception) {
+        return new ErrorResponse("error", "В работе сервера возникла ошибка.");
     }
 
     public static class ErrorResponse {
