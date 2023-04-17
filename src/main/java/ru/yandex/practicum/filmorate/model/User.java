b32323820b7684c3f8bf91a0d6ebdad3641ae63b
AVG <alex.glazyrin86@mail.ru>
2023-04-17 23:42:42 +0300

    создана новая структура проекта

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -21,8 +21,7 @@ public class User {
 
     private final Set<Integer> friends = new HashSet<>();
 
-    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"
-            ,message = "Неверно указан электронный адрес.")
+    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", message = "Неверно указан электронный адрес.")
     @NotBlank(message = "Неверно указан электронный адрес.")
     private final String email;
 
