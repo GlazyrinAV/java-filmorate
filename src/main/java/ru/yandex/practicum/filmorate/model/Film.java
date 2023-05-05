b344dfd581b6437d1e8d7ae1b78eaabcc83c9689
AVG <alex.glazyrin86@mail.ru>
2023-05-05 23:26:29 +0300

    feat - хранилище лайков перенесено в InMemoryFilmStorage - переименованы методы - добавлены методы IsExists в FilmService  и UserService - удалены нестандартные геттеры у Genre  и Rating

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,10 +11,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.HashSet;
-
 import java.util.List;
-import java.util.Set;
 
 @Data
 @EqualsAndHashCode(exclude = "id")
