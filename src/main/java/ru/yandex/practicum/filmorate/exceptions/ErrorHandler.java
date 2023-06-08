f71ae0a21415789aab9aebc167574f6b718e49b3
AVG <glazyrinav1986@yandex.ru>
2023-06-08 12:03:48 +0300

    feat - исправлены замечания по проекту

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -16,78 +16,72 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse noResultDataAccessException(NoResultDataAccessException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("no data returned", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse dataIntegrityViolationException(DataIntegrityViolationException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("data access error", exception.getMessage());
+        return sendErrorResponse("data access error", exception.getMessage());
     }
 
     @ExceptionHandler({UserAlreadyExistsException.class, FilmAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse userAlreadyExistsException(RuntimeException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler({UserNotFoundException.class, FilmNotFoundException.class, ReviewNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse entityNotFoundException(RuntimeException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse validationException(ValidationException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse illegalArgumentException(IllegalArgumentException exception) {
-        log.info("Получен неподходящий аргумент или аргумент неправильного типа");
-        return new ErrorResponse("error", "Получен неподходящий аргумент или аргумент неправильного типа");
+        return sendErrorResponse("error", "Получен неподходящий аргумент или аргумент неправильного типа");
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse otherException(RuntimeException exception) {
-        log.info("В работе сервера возникла ошибка.");
-        return new ErrorResponse("error", "В работе сервера возникла ошибка.");
+        return sendErrorResponse("error", "В работе сервера возникла ошибка.");
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public  ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getFieldErrors().toString());
+    public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler({LikeAlreadyExistsException.class, ReviewAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse entityAlreadyExistsException(RuntimeException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse friendAlreadyExistException(FriendAlreadyExistException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse likeNotFoundException(LikeNotFoundException exception) {
-        log.info(exception.getMessage());
-        return new ErrorResponse("error", exception.getMessage());
+        return sendErrorResponse("error", exception.getMessage());
+    }
+
+    private ErrorResponse sendErrorResponse(String errorType, String description) {
+        log.info(description);
+        return new ErrorResponse(errorType, description);
     }
 
     @Data
