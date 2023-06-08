70e1bef1ceb1b1fde2b92f17036d2d607bdd49e4
«KolesnikovaEV» <homyachokk96@yandex.ru>
2023-06-08 13:38:27 +0300

    Добавлена реализация удаления фильма и юзера

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -61,6 +61,7 @@ public class FilmController {
     public Collection<Film> findPopularFilms(@RequestParam(defaultValue = "10") int count) {
         return filmService.findPopular(count);
     }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteFilm(@PathVariable int id) {
